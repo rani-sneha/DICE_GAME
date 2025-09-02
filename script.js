197a0a15cf29f2ca5119dd909dd4d6f95b4956e5
Sneha Rani <sneharani0212@gmail.com>
2025-09-02 15:41:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var r1=Math.floor(Math.random() * 6 + 1);
+var r2=Math.floor(Math.random() * 6 + 1);
+document.querySelector(".img1").setAttribute("src", "images/dice" + r1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + r2 + ".png");
+if(r1>r2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}   
+else if(r2>r1){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}   
